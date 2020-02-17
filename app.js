32d54dff6096c55201a802b37d7ac349fdc55934
Arshavsky <pavel.a@taboola.com>
2020-02-17 22:30:25 +0200

    cors added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const wav = require('wav');
+const cors = require('cors');
 const app = express();
 const fs = require('fs');
 const multer = require('multer'); //use multer to upload blob data
@@ -23,6 +24,7 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
+app.use(cors());
 
 let counter = 0;
 // catch 404 and forward to error handler
